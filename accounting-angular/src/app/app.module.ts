b27d2da680cf17c0c88b6ffc20f3143ed40b5183
Fernando <fferreira12@gmail.com>
2017-08-11 13:19:13 -0300

    started add-transaction component

--- accounting-angular/src/app/app.module.ts
+++ accounting-angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddTransactionComponent } from './add-transaction/add-transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTransactionComponent
   ],
   imports: [
     BrowserModule
