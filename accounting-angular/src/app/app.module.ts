465b61f947bd0454ec30afe0d423849540b20d0d
Unknown <fferreira12@gmail.com>
2017-09-03 14:37:37 -0300

    Adds nav bar component

--- accounting-angular/src/app/app.module.ts
+++ accounting-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AddTransactionComponent } from './add-transaction/add-transaction.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTransactionComponent
+    AddTransactionComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
