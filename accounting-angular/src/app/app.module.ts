1e7996d3a45872ec8edef2201f9365f963ec8d64
Unknown <fferreira12@gmail.com>
2017-09-03 15:06:17 -0300

    Adds add account component

--- accounting-angular/src/app/app.module.ts
+++ accounting-angular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AddTransactionComponent } from './add-transaction/add-transaction.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AddAccountComponent } from './add-account/add-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTransactionComponent,
-    NavBarComponent
+    NavBarComponent,
+    AddAccountComponent
   ],
   imports: [
     BrowserModule
